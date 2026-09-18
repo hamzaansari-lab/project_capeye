e957a62b0d00c0a597dcb46668a07e96754ffc50
hamzaansari-lab <hamza.ansari@neuralnexuslab.com>
2026-09-18 10:41:42 +0530

    Sensor Mock Always Fails (No Success Path Tested)

--- sensor_read.cpp
+++ sensor_read.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int sensor()
-{
-    return -1;   
+int sensor(){
+    if(rand()%10<3) return -1;
+    return rand()%50+1;
 }
 
-int main()
-{
-    int distance = -1;
+int main(){
+    srand(time(0));
 
-    for (int i = 1; i <= 3; i++)
-    {
-        cout << "Attempt " << i << endl;
-
-        distance = sensor();
-
-        if (distance > 0)
-        {
-            cout << "Success: " << distance << " cm\n";
+    for(int i=1;i<=3;i++){
+        int d=sensor();
+        if(d>0){
+            cout<<"Success: "<<d<<" cm\n";
             break;
         }
-
-        cout << "Failed\n";
+        cout<<"Failed\n";
     }
-
-    if (distance == -1)
-        cout << "MEASURE FAILED";
-
-    return 0;
 }
