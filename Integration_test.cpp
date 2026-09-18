fba60c825c15c921e6fd86f0c2a46654b54b9b66
hamzaansari-lab <hamza.ansari@neuralnexuslab.com>
2026-09-18 11:51:28 +0530

    Integration Test

--- Integration_test.cpp
+++ Integration_test.cpp
@@ -3,17 +3,17 @@
 #include <ctime>
 using namespace std;
 
-int main() {
+int sensor()
+{return rand()%51;}
+string level(int d)
+{return d<10?"FULL":d<25?"HALF":"EMPTY";}
+bool send()
+{return rand()%2;}
+
+int main()
+{
     srand(time(0));
-
-    for(int i=0;i<20;i++) {
-        int d=rand()%51, b=rand()%101;
-
-        cout<<"WAKE\n";
-        cout<<"Distance: "<<d<<"cm\n";
-        cout<<"Battery: "<<b<<"%\n";
-        cout<<"Level: "<<(d<10?"FULL":d<25?"HALF":"EMPTY")<<"\n";
-        cout<<(b<20?"LOW BATTERY":rand()%2?"SEND OK":"SEND FAILED")<<"\n";
-        cout<<"SLEEP\n\n";
-    }
+    int d=sensor();
+    cout<<"WAKE\n"<<d<<"\n"<<level(d)<<"\n";
+    cout<<(send()?"SEND OK":"SEND FAILED")<<"\nSLEEP";
 }
